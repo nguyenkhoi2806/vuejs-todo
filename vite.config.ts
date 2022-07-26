b9c5d3d3dc570621120a0bf190862fb8291c44b3
nguyenkhoi2806 <lenguyenkhoi2806@gmail.com>
2022-07-26 22:55:05 +0700

    add vite config deploy

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import eslintPlugin from "vite-plugin-eslint";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(), eslintPlugin()],
+  base: "/vuejs-todo/",
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
