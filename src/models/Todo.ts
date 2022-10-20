d9c62f8335bf3495763ef2ef689bede2b2f893a9
nguyenkhoi2806 <lenguyenkhoi2806@gmail.com>
2022-10-20 14:38:00 +0700

    add file todo model

---
+++ src/models/Todo.ts
@@ -0,0 +1,25 @@
+import moment from "moment";
+interface ITodo {
+  id?: number;
+  name: string;
+  status?: boolean;
+  createdDate?: string;
+}
+
+class Todo implements ITodo {
+  id: number;
+  name: string;
+  status: boolean;
+  createdDate: string;
+
+  constructor(todo: ITodo) {
+    this.id = todo.id ? todo.id : Math.random();
+    this.name = todo.name;
+    this.status = todo.status ? todo.status : false;
+    this.createdDate = todo.createdDate
+      ? todo.createdDate
+      : moment().format("MMMM Do YYYY, h:mm:ss a");
+  }
+}
+
+export default Todo;
