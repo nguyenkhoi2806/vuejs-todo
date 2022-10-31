2e091c7eb2f6e1c2b41985f7da112ae04cef8fa2
nguyenkhoi2806 <lenguyenkhoi2806@gmail.com>
2022-10-31 16:00:57 +0700

    add teleport

--- src/components/Modal/Modal.vue
+++ src/components/Modal/Modal.vue
@@ -43,24 +43,26 @@ export default {
 </script>
 
 <template>
-  <div
-    class="fixed w-full h-full top-0 left-0 flex items-center justify-center z-10"
-  >
+  <teleport to="#modal">
     <div
-      class="absolute w-full h-full bg-gray-900 opacity-50"
-      @click="onClose"
-    ></div>
-    <div class="absolute max-h-full max-w-xl w-full" :class="maxWidth">
-      <div class="container bg-white overflow-hidden md:rounded">
-        <div class="px-4 py-4 w-full">
-          <div class="max-h-full">
-            <div class="items-center mb-2">
-              <h3>{{ title }}</h3>
+      class="fixed w-full h-full top-0 left-0 flex items-center justify-center z-10"
+    >
+      <div
+        class="absolute w-full h-full bg-gray-900 opacity-50"
+        @click="onClose"
+      ></div>
+      <div class="absolute max-h-full max-w-xl w-full" :class="maxWidth">
+        <div class="container bg-white overflow-hidden md:rounded">
+          <div class="px-4 py-4 w-full">
+            <div class="max-h-full">
+              <div class="items-center mb-2">
+                <h3>{{ title }}</h3>
+              </div>
+              <slot></slot>
             </div>
-            <slot></slot>
           </div>
         </div>
       </div>
     </div>
-  </div>
+  </teleport>
 </template>
