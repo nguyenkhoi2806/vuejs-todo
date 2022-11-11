6bc3eb84579770e49144d8f4ac1e045f6e3dc2f3
Khoi <lenguyenkhoi2806@gmail.com>
2022-11-11 09:59:55 +0700

    update ui loading component

--- src/components/Loading/Loading.vue
+++ src/components/Loading/Loading.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="loading" class="flex justify-center items-center mt-3 mb-3">
+  <div id="loading" class="flex justify-center items-center mt-5 mb-5">
     <div
       class="spinner-border animate-spin inline-block w-8 h-8 border-4 rounded-full"
       role="status"
