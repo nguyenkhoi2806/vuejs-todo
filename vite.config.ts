d6842d0c4572bf2e787b888266b2a8a845634f74
nguyenkhoi2806 <lenguyenkhoi2806@gmail.com>
2022-11-15 16:23:56 +0700

    edit pipeline

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,12 @@ import { fileURLToPath, URL } from "url";
 import { defineConfig } from "vite";
 import eslintPlugin from "vite-plugin-eslint";
 
+const ASSET_URL = process.env.ASSET_URL || "";
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(), eslintPlugin()],
+  base: `${ASSET_URL}`,
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
