49f39bef99c06f6c185a60af366ffa792d3ecca0
khoile <khoi.lenguyen@evolutionwellnesssolutionsg.onmicrosoft.com>
2024-06-19 10:55:30 +0700

    feat: update category selec

--- src/views/Home/Home.vue
+++ src/views/Home/Home.vue
@@ -3,6 +3,7 @@ import Loading from "@/components/Loading.vue";
 import Button from "@/components/Button.vue";
 
 import ProductCard from "./ProductCard.vue";
+import Search from "./Search.vue";
 
 export default {
   name: "Home",
@@ -10,6 +11,7 @@ export default {
     ProductCard,
     Loading,
     Button,
+    Search,
   },
   data() {
     return {
@@ -45,16 +47,7 @@ export default {
 <template>
   <div class="flex justify-between items-center w-full">
     <h2 class="text-2xl font-bold tracking-tight text-gray-900">My Products</h2>
-    <a class="pl-3 inline-block no-underline hover:text-black" href="#">
-      <svg
-        class="fill-current hover:text-black"
-        xmlns="http://www.w3.org/2000/svg"
-        width="24"
-        height="24"
-        viewBox="0 0 24 24">
-        <path d="M7 11H17V13H7zM4 7H20V9H4zM10 15H14V17H10z" />
-      </svg>
-    </a>
+    <Search />
   </div>
   <div
     class="mt-6 grid grid-cols-1 gap-x-6 gap-y-16 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-4 xl:gap-x-[30px]">
--- src/views/Home/Search.vue
+++ src/views/Home/Search.vue
@@ -2,6 +2,41 @@
 export default {
   name: "Search",
   components: {},
+  data: function () {
+    return {
+      categories: [],
+      categorySelected: "All",
+    };
+  },
+  created() {
+    this.fetchCategory();
+  },
+  methods: {
+    selectCategory(category) {
+      this.categorySelected = category;
+    },
+    async fetchCategory() {
+      return fetch("https://fakestoreapi.com/products/categories").then(
+        async (result) => {
+          const data = await result.json();
+          this.categories = ["All"].concat(data);
+        },
+      );
+    },
+  },
 };
 </script>
-<template></template>
+<template>
+  <div class="flex">
+    <button
+      v-for="category in categories"
+      :key="category"
+      type="button"
+      class="mr-2 border text-black p-3 rounded leading-none flex items-center"
+      :value="category"
+      :class="{ 'border-black': categorySelected === category }"
+      @click="selectCategory(category)">
+      {{ category }}
+    </button>
+  </div>
+</template>
